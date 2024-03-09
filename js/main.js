166cfab50a1553eb2047ae45d2d30381b4bfeb69
mh326 <m.hiroki326@gmail.com>
2024-03-09 23:04:00 +0900

    #5 feat: keep elapsed time even after leaving page

--- js/main.js
+++ js/main.js
@@ -85,7 +85,7 @@ class Timer {
             return;
         }
 
-        const endEpoch = Date.now() + this.#state.leftMilliseconds;
+        const endEpoch = Date.now() + Number(this.#state.leftMilliseconds);
         this.#state = {
             isStopped: false,
             endEpoch,
@@ -137,6 +137,22 @@ class Timer {
         const seconds = leftSeconds % 60;
         return `${zeroPad(minutes, 2)} : ${zeroPad(seconds, 2)}`;
     }
+
+    loadState() {
+        const leftMilliseconds = localStorage.getItem("leftMilliseconds");
+        if (leftMilliseconds != null) {
+            this.#state.leftMilliseconds = leftMilliseconds;
+        }
+    }
+
+    saveState() {
+        const leftMilliseconds = this.getLeftMilliseconds();
+        if (leftMilliseconds > 0) {
+            localStorage.setItem("leftMilliseconds", leftMilliseconds);
+        } else {
+            localStorage.removeItem("leftMilliseconds");
+        }
+    }
 }
 
 class TimerUI {
@@ -178,6 +194,7 @@ class TimerUI {
 
             const text = this.timer.format();
             this.writeToCanvas(text);
+            this.timer.saveState();
         };
 
         // Update UI and state periodically. This interval is how often the UI
@@ -276,6 +293,8 @@ window.onload = function () {
     setParam(timer, params, 'fontSize');
     setParam(timer, params, 'fontSizeTimeOver');
     setParam(timer, params, 'fontColor');
+
+    timer.timer.loadState();
 
     const btnStart = document.getElementById('btn-start');
     btnStart.addEventListener('click', () => {
